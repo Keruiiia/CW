ef19726fc5486568b4ba0a2934a70826c0d185c8
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-09-01 07:00:52 +0300

    9 day

---
+++ Python/7 kuy/7 kuy Basic Sequence Practice.py
@@ -0,0 +1,23 @@
+'''In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+
+Examples
+ 5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+ 7  -->  [0,  1,  3,  6,  10,  15,  21,  28]'''
+ 
+def sum_of_n(n):
+   if n < 0:
+       return [-sum(range(a)) for a in range(1, abs(n) + 2)]    
+   return [sum(range(a)) for a in range(1, n + 2)]
+	
+def test_sum_of_n():
+	assert sum_of_n(5) == [0, 1, 3, 6]
+	assert sum_of_n(3) = [0, 1, 3, 6]
+    assert sum_of_n(-4) ==[0, -1, -3, -6, -10]
+    assert sum_of_n(1) == [0, 1]
+	print("All tests passed")
