2da74e47b0abf7ef54aa822dd066bb7dc47ff275
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-09-03 23:57:00 +0300

    11 day 2

--- Python/7 kuy/7 kuy Substituting Variables Into Strings Padded Numbers.py
+++ Python/7 kuy/7 kuy Substituting Variables Into Strings Padded Numbers.py
@@ -4,3 +4,14 @@ The return value should equal "Value is VALUE" where value is a 5 digit padded n
 Example:
 
 solution(5)  # should return "Value is 00005"'''
+
+def solution(value):
+    return f'Value is {value:05d}'
+	
+
+def test_solution():
+	assert solution(0) == 'Value is 00000'
+    assert solution(5) =='Value is 00005'
+    assert solution(109) == 'Value is 00109'
+    assert solution(1204) == 'Value is 01204'
+	print("All tests passed")
