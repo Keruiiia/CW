f49966faa1eabba42a5dd4083f82e23cf97fb69c
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-09-06 23:59:06 +0300

    14 day 2

--- Python/8 kuy/8 kuy Holiday VI - Shark Pontoon.py
+++ Python/8 kuy/8 kuy Holiday VI - Shark Pontoon.py
@@ -1,8 +1,4 @@
 '''
-Your friend invites you out to a cool floating pontoon around 1km off the beach.
-Among other things, the pontoon has a huge slide that drops you out right into
-the ocean, a small way from a set of stairs used to climb out.
-
 As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
 
 You need to work out if the shark will get to you before you can get to the pontoon.
