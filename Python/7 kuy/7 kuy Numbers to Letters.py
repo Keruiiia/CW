3c773b74e0c98a7d6ea012239a5114b607e6c246
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-10-06 01:53:50 +0300

    43 day 2

--- Python/7 kuy/7 kuy Numbers to Letters.py
+++ Python/7 kuy/7 kuy Numbers to Letters.py
@@ -6,3 +6,22 @@ a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented
 All inputs will be valid.
 '''
 
+def switcher(arr):
+    letters = (chr(x) for x in range(97, 123))
+    digits = (str(d) for d in range(26, 0, -1))
+    d = {k: v for k, v in zip(digits, letters)}
+    d["27"] = '!'
+    d["28"] = '?'
+    d["29"] = ' '
+    return ''.join(d[x] for x in arr)
+	
+
+def test_switcher():
+    assert switcher(['24', '12', '23', '22', '4', '26', '9', '8']) == 'codewars'
+    assert switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']) == 'btswmdsbd kkw'
+    assert switcher(['4', '24']) == 'wc'
+    assert switcher(['12']) == 'o'
+	print("All tests passed!")
+	
+	
+test_switcher()
