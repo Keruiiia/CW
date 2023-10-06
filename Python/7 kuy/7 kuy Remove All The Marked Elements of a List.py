0a207fa55f2707561712988149dcd961483ad35f
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-10-07 00:53:59 +0300

    44 day 2

--- Python/7 kuy/7 kuy Remove All The Marked Elements of a List.py
+++ Python/7 kuy/7 kuy Remove All The Marked Elements of a List.py
@@ -5,3 +5,7 @@ Examples (input -> output):
 * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
 * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
 '''
+
+class List:
+    def remove_(self, integer_list, values_list):
+        return [x for x in integer_list if x not in values_list]
