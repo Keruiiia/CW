65e0849dfa1c7d89b4b741877dc78dd68d98c2ca
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-10-21 21:23:05 +0300

    55 day 2

--- Python/7 kuy/7 kuy lucky number.py
+++ Python/7 kuy/7 kuy lucky number.py
@@ -7,3 +7,23 @@ If the sum of all digits is 0 or multiple of 9 then the number is lucky.
 Function will return true for lucky numbers and false for others.
 '''
 
+def is_lucky(n):
+    if n == 0:
+        return True
+    s = 0
+    while n > 0:
+        ost = n % 10
+        s += ost
+        n //= 10
+    return s % 9 == 0
+	
+# smart solution
+def is_lucky(n):
+    return n % 9 == 0
+
+	
+def test_is_lucky():
+	assert is_lucky(1892376) == True
+    assert is_lucky(189237) == False
+    assert is_lucky(18922314324324234423437) == False
+	print("All tests passed!")
