02c6e67b926bb30bba73e85ea55dc6059d7a2cec
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-10-30 21:26:46 +0300

    63 day 2

---
+++ Python/8 kuy/8 kuy Ensure question.py
@@ -0,0 +1,22 @@
+'''
+Given a string, write a function that returns the string with a
+question mark ("?") appends to the end, unless the original string
+ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+'''
+
+def ensure_question(s):
+    if not s:
+        return '?'
+    elif s[-1] != "?":
+        return s + "?"
+    return s
+	
+#smart solution
+
+def ensure_question(s):
+    return s.rstrip('?') + '?'
