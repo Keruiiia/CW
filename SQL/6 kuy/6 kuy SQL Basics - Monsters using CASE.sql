ff9f26131b517c2df8f55d43ae79da6e58253bac
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-11-12 23:59:08 +0300

    77 day 2

--- SQL/6 kuy/6 kuy SQL Basics - Monsters using CASE.sql
+++ SQL/6 kuy/6 kuy SQL Basics - Monsters using CASE.sql
@@ -26,3 +26,13 @@ The IDs on the tables match to make a full monster. For heads, arms, legs and ta
 For the species, if the monster has more heads than arms, more tails than legs, or both,
 it is a 'BEAST' else it is a 'WEIRDO'. This needs to be captured in the species column.
 '''
+
+SELECT
+	id,
+	top_half.heads,
+	top_half.arms,
+	bottom_half.legs,
+	bottom_half.tails,
+	CASE
+		WHEN 
+	
