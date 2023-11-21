354fbfc4cf709549bcd6d472c697459d51424792
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-11-21 20:44:58 +0300

    86 day 2

--- Python/7 kuy/7 kuy Correct the time-string.py
+++ Python/7 kuy/7 kuy Correct the time-string.py
@@ -15,3 +15,29 @@ Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 I have created other katas. Have a look if you like coding and challenges.
 '''
+
+import re
+
+def time_correct(s):
+    if s is None:
+        return None
+    if s == '':
+        return ''
+    pattern = r'^\d{2}:\d{2}:\d{2}$'
+    if not re.match(pattern, s):
+        return None
+    h, m, s = [int(x) for x in s.split(':')]
+    sum_sec = h*3600 + m*60 + s
+    h, m, s = sum_sec // 3600 % 24, sum_sec % 3600 // 60, sum_sec % 60
+    return ':'.join(str(i).zfill(2) for i in (h, m, s))
+	
+#smart_solution
+
+def time_correct(t):
+    if not t: return t
+    try:
+        h, m, s = map(int, t.split(':'))
+        if s >= 60: s -= 60; m += 1
+        if m >= 60: m -= 60; h += 1
+        return '%02d:%02d:%02d' % (h % 24, m, s)
+    except: pass
