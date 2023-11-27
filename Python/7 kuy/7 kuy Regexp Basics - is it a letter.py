3f71bc5d252c4c5b69edaf9c3f483dbe89ef9f49
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-11-27 20:22:55 +0300

    91 day 2

--- Python/7 kuy/7 kuy Regexp Basics - is it a letter.py
+++ Python/7 kuy/7 kuy Regexp Basics - is it a letter.py
@@ -1,3 +1,7 @@
 '''
 Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
 '''
+
+import re
+def is_letter(s):
+    return bool(re.fullmatch("[A-Za-z]",s))
