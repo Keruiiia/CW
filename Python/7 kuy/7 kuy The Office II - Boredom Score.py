bf3e1ec311baf51efbc3f99c5845ed1605ebfcf3
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-11-29 21:31:00 +0300

    93 day 2

--- Python/7 kuy/7 kuy The Office II - Boredom Score.py
+++ Python/7 kuy/7 kuy The Office II - Boredom Score.py
@@ -24,3 +24,48 @@ Depending on the cumulative score of the team, return the appropriate sentiment:
 100 or over: 'party time!!'
 '''
 
+def boredom(staff):
+    d = {
+        'accounts': 1,
+        'finance': 2,
+        'canteen': 10,
+        'regulation': 3,
+        'trading': 6,
+        'change': 6,
+        'IS': 8,
+        'retail': 5,
+        'cleaning': 4,
+        'pissing about': 25
+    }
+    res = 0
+    for name, depatment in staff.items():
+        res += d[depatment]
+    if res <= 80:
+        return 'kill me now'
+    elif 80 < res < 100:
+        return 'i can handle this'
+    else:
+        return 'party time!!'
+		
+#smat_colution
+
+boredom_scores = {
+    'accounts': 1,
+    'finance': 2 ,
+    'canteen': 10 ,
+    'regulation': 3 ,
+    'trading': 6 ,
+    'change': 6,
+    'IS': 8,
+    'retail': 5,
+    'cleaning': 4,
+    'pissing about': 25,
+}
+
+def boredom(staff):
+    score = sum(map(boredom_scores.get, staff.values()))
+    return (
+        'kill me now' if score <= 80 else
+        'i can handle this' if score < 100 else
+        'party time!!'
+    )
