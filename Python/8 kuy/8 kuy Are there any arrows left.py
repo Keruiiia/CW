ded7abcd420963e1d74828f7c126916bce4cbadd
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-11-30 22:33:12 +0300

    94 day 2

--- Python/8 kuy/8 kuy Are there any arrows left.py
+++ Python/8 kuy/8 kuy Are there any arrows left.py
@@ -9,3 +9,9 @@ If an arrow in the quiver does not have a damaged status, it means it's new.
 The expected result is a boolean, indicating whether you have any good arrows left.
 '''
 
+'''
+[{'range': 5}, {'range': 10, 'damaged': True}, {'damaged': True}])
+'''
+
+def any_arrows(arrows):
+    return any(not i.get("damaged", False) for i in arrows)
