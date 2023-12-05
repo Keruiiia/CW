287dafd8f41a0175847071c4d9cea17173232c84
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-05 20:16:00 +0300

    98 day 2

--- Python/7 kuy/7 kuy Special Number (Special Numbers Series #5).py
+++ Python/7 kuy/7 kuy Special Number (Special Numbers Series #5).py
@@ -3,3 +3,11 @@ A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
 
 Given a number determine if it special number or not .
 '''
+
+def special_number(number):
+    return ('Special!!', 'NOT!!')[len({'6', '7', '8', '9'} & set(str(number))) > 0]
+	
+#smart_solution
+
+def special_number(n):
+    return "Special!!" if max(str(n)) <= "5" else "NOT!!"
