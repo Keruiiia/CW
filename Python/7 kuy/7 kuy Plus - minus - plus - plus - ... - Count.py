e9fe2961fb40b702d3da395faac3303ddae92279
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-06 21:15:33 +0300

    99 day 2

--- Python/7 kuy/7 kuy Plus - minus - plus - plus - ... - Count.py
+++ Python/7 kuy/7 kuy Plus - minus - plus - plus - ... - Count.py
@@ -19,3 +19,16 @@ const arr = [1, -3, -4, 0, 5];
 
 catchSignChange(arr) == 2;
 '''
+
+def catch_sign_change(lst):
+    res = list(map(lambda el: 1 if el >= 0 else 0, lst))
+    counter = 0
+    for i in range(1, len(res)):
+        if res[i] != res[i-1]:
+            counter += 1
+    return counter
+	
+#smart_solution
+
+def catch_sign_change(lst):
+    return sum( (x >= 0) != (y >= 0) for x, y in zip(lst, lst[1:]) )
