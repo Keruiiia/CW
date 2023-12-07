f7bae3730c653a24a0c6ad962579c6e3ee90a14c
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-07 22:47:48 +0300

    100 day 2

--- Python/8 kuy/8 kuy Playing with cubes II.py
+++ Python/8 kuy/8 kuy Playing with cubes II.py
@@ -7,3 +7,14 @@ Correct the code so negative values will be switched to positive ones!
 
 The constructor taking no arguments should assign 0 to Cube's Side property.
 '''
+class Cube(object):
+    def __init__(self, side=0):
+        self.set_side(side)
+    
+    def get_side(self):
+        """Return the side of the Cube"""
+        return self._side
+
+    def set_side(self, new_side):
+        """Set the value of the Cube's side."""
+        self._side = abs(new_side)
