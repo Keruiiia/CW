aa5fe69c19b5279c38aa670b6d3ec3a61944de82
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-11 22:00:29 +0300

    103 day

---
+++ Python/7 kuy/7 kuy Area of a Circle.py
@@ -0,0 +1,8 @@
+'''
+Complete the function which will return the area of a circle with the given radius.
+
+Returned value is expected to be accurate up to tolerance of 0.01.
+
+If the radius is not positive, raise ValueError.
+'''
+
