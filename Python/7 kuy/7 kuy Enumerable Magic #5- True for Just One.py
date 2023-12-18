7ac0f11c2e73b2cc4219f1a2d62aa10867304cfd
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-18 21:57:07 +0300

    109 day

---
+++ Python/7 kuy/7 kuy Enumerable Magic #5- True for Just One.py
@@ -0,0 +1,7 @@
+'''
+Create a function called one that accepts two params:
+
+a sequence
+a function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+'''
