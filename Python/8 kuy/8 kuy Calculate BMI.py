03f6c7ed78938914f71598409f16337f5a14bd64
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-19 22:48:31 +0300

    110 day 2

--- Python/8 kuy/8 kuy Calculate BMI.py
+++ Python/8 kuy/8 kuy Calculate BMI.py
@@ -9,3 +9,19 @@ if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"
 '''
+def bmi(weight, height):
+    bmi = weight / height**2
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25.0:
+        return "Normal"
+    elif bmi <= 30.0:
+        return "Overweight"
+    elif bmi > 30:
+        return "Obese"
+		
+#smart_solution
+
+def bmi(weight, height):
+    b = weight / height ** 2
+    return ['Underweight', 'Normal', 'Overweight', 'Obese'][(b > 30) + (b > 25) + (b > 18.5)]
