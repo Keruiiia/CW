39bb3cf8b43e884f9fc75cf662ea9505f157adf1
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-24 17:16:55 +0300

    115 day

---
+++ Python/7 kuy/7 kuy Disarium Number (Special Numbers Series #3).py
@@ -0,0 +1,9 @@
+'''
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+disariumNumber(89) ==> return "Disarium !!"
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+disariumNumber(564) ==> return "Not !!"
+Explanation:
+Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"
+'''
