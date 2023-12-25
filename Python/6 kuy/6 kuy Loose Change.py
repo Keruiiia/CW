7f4aadc6b92cb15d9336a2e6411272e29caa655d
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2023-12-25 22:55:22 +0300

    116 day

--- Python/6 kuy/6 kuy Loose Change.py
+++ Python/6 kuy/6 kuy Loose Change.py
@@ -10,3 +10,13 @@ loose_change(56)    ==>  {'Nickels': 1, 'Pennies': 1, 'Dimes': 0, 'Quarters': 2}
 loose_change(-435)  ==>  {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
 loose_change(4.935) ==>  {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 0}
 '''
+def loose_change(cents):
+    coins = {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+    if cents <= 0:
+        return coins
+    coins['Quarters'] = cents // 25
+    coins['Dimes'] = cents % 25 // 10
+    coins['Nickels'] = cents % 25 % 10 // 5
+    coins['Pennies'] = cents % 25 % 10 % 5 // 1
+    
+    return coins
