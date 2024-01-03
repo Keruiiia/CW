47c10d7714031afde14eea1196424fab1fb75d0e
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-03 22:09:43 +0300

    122 day 2

--- Python/7 kuy/7 kuy Move 10.py
+++ Python/7 kuy/7 kuy Move 10.py
@@ -5,3 +5,16 @@ If it goes past 'z', start again at 'a'.
 
 Input will be a string with length > 0.
 '''
+def move_ten(st):
+    res = ''
+    for char in st:
+        n = (ord(char) + 10)
+        if n > 122:
+            n = n  % 122 + 96
+        res += chr(n)
+    return res
+	
+A = 'abcdefghijklmnopqrstuvwxyz'
+
+def move_ten(s):
+    return ''.join(s.translate(str.maketrans(A, A[10:] + A[:10])))
