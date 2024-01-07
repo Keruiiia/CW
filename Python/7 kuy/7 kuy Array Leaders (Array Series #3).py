1e5ec1f6292ec530b3928aff040c4eb8f83b3c4a
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-07 20:31:35 +0300

    126 day 2

--- Python/7 kuy/7 kuy Array Leaders (Array Series #3).py
+++ Python/7 kuy/7 kuy Array Leaders (Array Series #3).py
@@ -7,3 +7,5 @@ Explanation:
 
 Note : The last element 2 is greater than the sum of its right elements (abstract zero)
 '''
+def array_leaders(numbers):
+    return [n for (i,n) in enumerate(numbers) if n>sum(numbers[(i+1):])]
