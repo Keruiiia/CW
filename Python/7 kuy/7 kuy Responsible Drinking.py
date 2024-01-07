f4847c88ab5453e2b5600a7676586262f30c01f9
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-08 00:15:49 +0300

    127 day 2

--- Python/7 kuy/7 kuy Responsible Drinking.py
+++ Python/7 kuy/7 kuy Responsible Drinking.py
@@ -11,3 +11,9 @@ because you drank one standard drink
 "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
 because you drank ten standard drinks
 '''
+def hydrate(drink_string): 
+    res = 0
+    for char in drink_string:
+        if char.isdigit():
+            res += int(char)
+    return f"{res} glasses of water" if res > 1 else "1 glass of water"
