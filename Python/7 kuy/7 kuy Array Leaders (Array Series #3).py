89319c8fe91a7ca752f1be1b77a58d5254e4e22b
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-07 20:24:35 +0300

    126 day

---
+++ Python/7 kuy/7 kuy Array Leaders (Array Series #3).py
@@ -0,0 +1,9 @@
+'''
+arrayLeaders ({16, 17, 4, 3, 5, 2}) ==> return {17, 5, 2}
+Explanation:
+17 is greater than the sum all the elements to its right side
+
+5 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero)
+'''
