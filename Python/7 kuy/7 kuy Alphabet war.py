f23fbe203236c854b14163c35d48258dddae17e9
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-11 00:10:12 +0300

    130 day 2

--- Python/7 kuy/7 kuy Alphabet war.py
+++ Python/7 kuy/7 kuy Alphabet war.py
@@ -25,3 +25,18 @@ AlphabetWar("zdqmwpbs"); //=> Let's fight again!
 AlphabetWar("zzzzs");    //=> Right side wins!
 AlphabetWar("wwwwwwz");  //=> Left side wins!
 '''
+
+def alphabet_war(fight):
+    left = {
+         'w': 4, 'p': 3, 'b': 2, 's': 1
+    }
+    right = {
+         'm': 4, 'q': 3, 'd': 2, 'z': 1
+    }
+    res = 0
+    for char in fight:
+        res -= left.get(char, 0)
+        res += right.get(char, 0)
+    if not res:
+        return "Let's fight again!"
+    return ('Left side wins!', 'Right side wins!')[res > 0]
