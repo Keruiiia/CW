537c1ced40d264aece8141ab35b8a11bf0a79a8f
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-12 00:23:08 +0300

    131 day

---
+++ Python/7 kuy/7 kuy Chain me.py
@@ -0,0 +1,9 @@
+'''
+The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+def add10(x): return x + 10
+def mul30(x): return x * 30
+
+chain(50, [add10, mul30])
+# returns 1800
+'''
