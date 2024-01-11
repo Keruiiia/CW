feeacfd9ad86c7efb7440aad884264d2a18a983c
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-12 00:27:34 +0300

    131 day 2

--- Python/7 kuy/7 kuy Chain me.py
+++ Python/7 kuy/7 kuy Chain me.py
@@ -7,3 +7,9 @@ def mul30(x): return x * 30
 chain(50, [add10, mul30])
 # returns 1800
 '''
+def chain(init_val, functions):
+    if not functions:
+        return init_val
+    for f in functions:
+        init_val = f(init_val)
+    return init_val
