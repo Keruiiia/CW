42a5387cd8678dc115c03ac6dd73fd9ca40cca40
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-14 23:13:58 +0300

    133 day

---
+++ Python/7 kuy/7 Kuy Exclamation marks series #13 Count the number of exclamation marks and question marks, return the product.py
@@ -0,0 +1,15 @@
+'''
+Count the number of exclamation marks and question marks, return the production.
+
+Examples
+""          --->   0
+"!"         --->   0
+"!ab? ?"    --->   2
+"!!"        --->   0
+"!??"       --->   2
+"!???"      --->   3
+"!!!??"     --->   6
+"!!!???"    --->   9
+"!???!!"    --->   9
+"!????!!!?" --->  20
+'''
