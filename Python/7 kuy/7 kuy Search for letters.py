9b97b3b178a37daf7d9bf394654f43d8a325c1b9
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-16 09:10:08 +0300

    135 day 2

--- Python/7 kuy/7 kuy Search for letters.py
+++ Python/7 kuy/7 kuy Search for letters.py
@@ -8,3 +8,22 @@ So if an 'a' or an 'A' appears anywhere in the input string (any number of times
 set the first character of the output string to '1', otherwise to '0'. if 'b' or 'B' appears
 in the string, set the second character to '1', and so on for the rest of the alphabet.
 '''
+def change(st):
+    draft = 'abcdefghijklmnopqrstuvwxyz'
+    res = ''
+    st = st.lower()
+    for char in draft:
+        if char in st:
+            res += '1'
+        else:
+            res += '0'
+    return res
+	
+#smart_solution
+
+from string import ascii_lowercase
+
+
+def change(st):
+    source = st.lower()
+    return ''.join('1' if q in source else '0' for q in ascii_lowercase)
