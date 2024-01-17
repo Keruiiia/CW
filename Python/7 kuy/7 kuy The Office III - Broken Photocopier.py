b63f500fe37b37f288e7ea925a2d2b15e1b2734d
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-17 19:00:53 +0300

    136 day 2

--- Python/7 kuy/7 kuy The Office III - Broken Photocopier.py
+++ Python/7 kuy/7 kuy The Office III - Broken Photocopier.py
@@ -5,3 +5,17 @@ Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
 
 Given a string of binary, return the version the photocopier gives you as a string.
 '''
+def broken(inp):
+    res = ''
+    for char in inp:
+        if char == '0':
+            res += '1'
+        else:
+            res += '0'
+    return res
+
+#smart_solution
+
+def broken(inp):
+    """Replace each '0' with '1' and vice versa."""
+    return inp.translate(str.maketrans("01", "10"))
