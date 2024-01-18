398dd7434bd7401dd9c59498db128faaab667aed
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-19 00:04:28 +0300

    138 day 2

--- Python/7 kuy/7 kuy Return the Missing Element.py
+++ Python/7 kuy/7 kuy Return the Missing Element.py
@@ -5,3 +5,13 @@ Examples:
 [0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
 [9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
 '''
+
+def get_missing_element(seq): 
+    res = set(range(10)) - set(seq)
+    return list(res)[0]
+	
+
+#smart_solution
+
+def get_missing_element(seq): 
+    return 45 - sum(seq)
