d71510c20e44fad40fb267a02a787205d78d20dd
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-19 00:00:17 +0300

    138 day

---
+++ Python/7 kuy/7 kuy Return the Missing Element.py
@@ -0,0 +1,7 @@
+'''
+Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+Examples:
+[0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+[9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
+'''
