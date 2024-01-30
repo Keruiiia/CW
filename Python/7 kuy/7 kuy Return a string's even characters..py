ab3d8e107a492447b482f5879a8e631bbfff8f39
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-01-31 00:34:01 +0300

    150 day 2

--- Python/7 kuy/7 kuy Return a string's even characters..py
+++ Python/7 kuy/7 kuy Return a string's even characters..py
@@ -7,3 +7,15 @@ For example:
 "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
 "a"             --> "invalid string"
 '''
+def even_chars(st): 
+    if len(st) < 2 or len(st) > 100:
+        return 'invalid string'
+    res = []
+    for i in range(1, len(st), 2):
+        res.append(st[i])
+    return res
+	
+#smart_solution
+
+def even_chars(st): 
+    return list(st[1::2]) if 2<=len(st)<=100 else "invalid string" 
