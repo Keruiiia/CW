c41004fe2f0c377b596147a695b3636ab991e7ef
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-09 00:00:29 +0300

    159 day

---
+++ Python/7 kuy/7 kuy Very Even Numbers..py
@@ -0,0 +1,12 @@
+'''
+Write a function that returns true if the number is a "Very Even" number.
+
+If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+
+Examples
+number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+'''
