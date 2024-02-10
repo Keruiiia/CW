230589d7ef4859d6694b9420c6f766f52800468f
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-10 21:20:24 +0300

    160 day 2

--- Python/8 kuy/8 kuy Geometry Basics Circle Circumference in 2D.py
+++ Python/8 kuy/8 kuy Geometry Basics Circle Circumference in 2D.py
@@ -7,3 +7,7 @@ Write a function calculating circumference of a Circle.
 
 Tests round answers to 6 decimal places.
 '''
+from math import pi
+
+def circle_circumference(circle):
+    return round(2 * circle.radius * pi, 6)
