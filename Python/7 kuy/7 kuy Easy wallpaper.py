eeabbbca7c1554a5d0095bcf9d94082602cda64d
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-14 00:19:43 +0300

    164 day

---
+++ Python/7 kuy/7 kuy Easy wallpaper.py
@@ -0,0 +1,22 @@
+'''
+Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
+
+Example:
+wallpaper(4.0, 3.5, 3.0) should return "ten"
+
+wallpaper(0.0, 3.5, 3.0) should return "zero"
+
+Notes:
+all rolls (even with incomplete width) are put edge to edge
+
+0 <= l, w, h (floating numbers); it can happens that w * h * l is zero
+
+the integer r (number of rolls) will always be less or equal to 20
+
+FORTH: the number of rolls will be a positive or null integer (not a plain English word; this number can be greater than 20)
+
+In Coffeescript, Javascript, Python, Ruby and Scala the English numbers are preloaded and can be accessed as:
+
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+For other languages it is not preloaded and you can instead copy the above list if you desire.
+'''
