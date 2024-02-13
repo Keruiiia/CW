78d956f0f92fb7e264c04fe4054aa187351a596a
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-14 00:20:45 +0300

    164 day 2

--- Python/7 kuy/7 kuy Easy wallpaper.py
+++ Python/7 kuy/7 kuy Easy wallpaper.py
@@ -20,3 +20,11 @@ In Coffeescript, Javascript, Python, Ruby and Scala the English numbers are prel
 numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 For other languages it is not preloaded and you can instead copy the above list if you desire.
 '''
+from math import ceil
+
+numbers = {0:"zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 
+9: "nine", 10: "ten", 11:"eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 
+16:"sixteen", 17:"seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty"}
+
+def wallpaper(l, w, h):
+    return "zero" if w*l==0 else numbers[ceil((2*l+2*w) * h * 1.15 / 5.2)]
