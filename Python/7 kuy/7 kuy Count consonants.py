3e4dd64c716c88ad1cbec9a0bd34fc41eae85bbd
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-19 01:00:10 +0300

    169 day 2

--- Python/7 kuy/7 kuy Count consonants.py
+++ Python/7 kuy/7 kuy Count consonants.py
@@ -4,3 +4,8 @@ Complete the function that takes a string of English-language text and returns t
 Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
 '''
 
+def consonant_count(s):
+    return len([char for char in s.lower() if char in 'qwrtyplkjhgfdszxcvbnm'])
+	
+def consonant_count(str):
+    return sum(1 for c in str if c.isalpha() and c.lower() not in "aeiou")
