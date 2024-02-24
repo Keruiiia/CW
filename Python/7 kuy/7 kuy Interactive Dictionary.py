0e7d248360150b7100f6b7e327a234ffda8b458f
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-24 21:38:03 +0300

    174 day 2

--- Python/7 kuy/7 kuy Interactive Dictionary.py
+++ Python/7 kuy/7 kuy Interactive Dictionary.py
@@ -13,3 +13,13 @@ Can't find entry for Banana
 Good luck and happy coding!
 '''
 
+class Dictionary(object):
+    def __init__(self):
+        self.my_dict = {}
+
+    def look(self, key):
+        return self.my_dict.get(key, "Can't find entry for {}".format(key))
+
+    def newentry(self, key, value):
+        """ new_entry == PEP8 (forced by Codewars) """
+        self.my_dict[key] = value
