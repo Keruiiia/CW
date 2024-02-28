9851f3db0d40a43f6b48c94ee39b069c215fb0ec
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-02-28 22:53:18 +0300

    178 day

---
+++ Python/7 kuy/7 kuy Linked Lists - Get Nth Node.py
@@ -0,0 +1,11 @@
+'''
+Implement a GetNth() function that takes a linked list and an integer index and returns
+the node stored at the Nth index position. GetNth() uses the C numbering convention that
+the first node is index 0, the second is index 1, ... and so on.
+
+So for the list 42 -> 13 -> 666, GetNth(1) should return Node(13);
+
+getNth(1 -> 2 -> 3 -> null, 0).data === 1
+getNth(1 -> 2 -> 3 -> null, 1).data === 2
+'''
+
