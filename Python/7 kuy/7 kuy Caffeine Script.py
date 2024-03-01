f2a0c7e3e6071ba6ff04390c63fdea676db61f27
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-01 21:20:18 +0300

    180 day 2

--- Python/7 kuy/7 kuy Caffeine Script.py
+++ Python/7 kuy/7 kuy Caffeine Script.py
@@ -16,3 +16,10 @@ Examples
 12  -->  "CoffeeScript"
 '''
 
+def caffeine_buzz(n):
+    if n % 3 == 0 and n % 4 == 0:
+        return "CoffeeScript" if n % 2 == 0 else "Coffee"
+    elif n % 3 == 0:
+        return "JavaScript" if n % 2 == 0 else "Java"
+    else:
+        return "mocha_missing!"
