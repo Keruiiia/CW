1218615d18f0c6b8c220e79ae3eb766ed4d095f1
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-06 00:19:09 +0300

    185 day 2

--- Python/6 kuy/6 kuy All Star Code Challange 15.py
+++ Python/6 kuy/6 kuy All Star Code Challange 15.py
@@ -6,3 +6,13 @@ Note The original string should be included in the output array.
 The order matters. Each element of the output array should be the rotated version of the previous element.
 The output array SHOULD be the same length as the input string. The function should return an empty array with a 0 length string, '', as input.
 '''
+def rotate(s):
+    if not s:
+        return []
+    res = [s[1:] + s[0]]
+    for _ in range(len(s) - 1):
+        res.append(res[-1][1:] + res[-1][0])
+    return res
+	
+def rotate(str_):
+    return [str_[i + 1:] + str_[:i + 1] for i in range(len(str_))]
