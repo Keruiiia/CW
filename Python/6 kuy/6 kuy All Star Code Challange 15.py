82262a093aa1e6f39e882193598f505933ada221
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-06 00:13:39 +0300

    185 day

---
+++ Python/6 kuy/6 kuy All Star Code Challange 15.py
@@ -0,0 +1,8 @@
+'''
+Create a function named rotate() that accepts a string argument and returns an array of strings with each letter from the input string being rotated to the end.
+
+rotate(Hello)  = [elloH, lloHe, loHel, oHell, Hello]
+Note The original string should be included in the output array.
+The order matters. Each element of the output array should be the rotated version of the previous element.
+The output array SHOULD be the same length as the input string. The function should return an empty array with a 0 length string, '', as input.
+'''
