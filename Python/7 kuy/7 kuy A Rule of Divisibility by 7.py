8bfa43ed947e038cc85a8c67a6c087ae26ae463c
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-05 23:05:27 +0300

    184 day

---
+++ Python/7 kuy/7 kuy A Rule of Divisibility by 7.py
@@ -0,0 +1,12 @@
+'''
+Your task is to return to the function seven(m) (m integer >= 0) an array (or a pair, depending on the language) of numbers, the first being the last number m with at most 2 digits obtained by your function (this last m will be divisible or not by 7), the second one being the number of steps to get the result.
+
+Forth Note:
+Return on the stack number-of-steps, last-number-m-with-at-most-2-digits 
+
+Examples:
+seven(371) should return [35, 1]
+seven(1603) should return [7, 2]
+seven(477557101) should return [28, 7]
+'''
+
