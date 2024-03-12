04c6266cf992cf10e2595707a97d7940ac1380cc
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-13 00:10:18 +0300

    191 day 2

--- Python/7 kuy/7 kuy Well of Ideas - Harder Version.py
+++ Python/7 kuy/7 kuy Well of Ideas - Harder Version.py
@@ -22,3 +22,7 @@ def well(arr):
     else:
         return 'I smell a series!'
 		
+
+def well(arr):
+    good_ideas = str(arr).lower().count('good')
+    return 'I smell a series!' if (good_ideas > 2) else 'Fail!' if not(good_ideas) else 'Publish!'
