bed53d4ad04e631fb1421274a476c0677ac3ea18
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-15 00:16:02 +0300

    193 day 2

--- Python/7 kuy/7 kuy Snail crawls up.py
+++ Python/7 kuy/7 kuy Snail crawls up.py
@@ -10,3 +10,20 @@ The distance that the snail slides down during the night (meters)
 Calculate number of day when the snail will reach the top of the column.
 '''
 
+def snail(column, day, night):
+    days = 0
+    while column > 0:
+        column -= day
+        if column <= 0:
+            break
+        column += night
+        days += 1
+    return days + 1
+	
+def snail(column, day, night):
+    speed = day - night
+    days = 1
+    while column - day > 0:
+        day += speed
+        days += 1
+    return days
