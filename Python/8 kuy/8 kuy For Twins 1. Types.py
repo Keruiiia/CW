8c8cc24a7e0115156ff578b249ca66e42eb7d32a
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-14 23:49:01 +0300

    192 day

---
+++ Python/8 kuy/8 kuy For Twins 1. Types.py
@@ -0,0 +1,9 @@
+'''
+Write a function that will accept two parameters: variable and type and check
+if type of variable is matching type. Return true if types match or false if not.
+
+Examples:
+42, "int"    --> True
+"42", "int"  --> False
+'''
+
