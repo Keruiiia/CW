9d32aa40f747b88a3c3df56c42c26f67f470273d
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-21 01:35:21 +0300

    198 day 2

--- Python/7 kuy/7 kuy Minimum Steps (Array Series #6).py
+++ Python/7 kuy/7 kuy Minimum Steps (Array Series #6).py
@@ -1,3 +1,7 @@
 '''
-Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
+Given an array of N integers, you have to find how many times you have to add up
+the smallest numbers in the array until their Sum becomes greater or equal to K.
 '''
+
+def minimum_steps(numbers, value):
+    return sum(sum(sorted(numbers)[:i+1]) < value for i in range(len(numbers)))
