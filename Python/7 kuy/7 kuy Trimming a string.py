1842a511359700b846106b3a636563a1490a442e
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-24 23:57:02 +0300

    201 day 2

--- Python/7 kuy/7 kuy Trimming a string.py
+++ Python/7 kuy/7 kuy Trimming a string.py
@@ -1,5 +1,6 @@
 '''
-Create a function that will trim a string (the first argument given) if it is longer than the requested maximum string length (the second argument given). The result should also end with "..."
+Create a function that will trim a string (the first argument given)
+if it is longer than the requested maximum string length (the second argument given). The result should also end with "..."
 
 These dots at the end also add to the string length.
 
@@ -15,4 +16,5 @@ e.g. trim("He", 1) should return "H...", because 1 <= 3
 
 Requested maximum length will be greater than 0. Input string will not be empty.
 '''
-
+def trim(phrase, size):
+    raise NotImplementedError("TODO: trim")
