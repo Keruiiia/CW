7f50d456faa4eacfa596f1f3ccc7fd69946a3fd8
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-28 01:29:59 +0300

    205 day

---
+++ Python/8 kuy/8 kuy Simple Fun #352 Reagent Formula.py
@@ -0,0 +1,12 @@
+'''
+You are given a integer array formula. Array contains only digits 1-8 that represents
+material 1-8. Your task is to determine if the formula is valid. Returns true if it's valid, false otherwise.
+
+Example
+For formula = [1,3,7], The output should be true.
+
+For formula = [7,1,2,3], The output should be false.
+
+For formula = [1,3,5,7], The output should be false.
+'''
+
