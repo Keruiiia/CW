46da8b50a3db4e7706a559f91722379ef9c2fe5d
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-03-29 00:26:34 +0300

    206 day 2

--- Python/6 kuy/6 kuy Round by 0.5 steps.py
+++ Python/6 kuy/6 kuy Round by 0.5 steps.py
@@ -12,3 +12,15 @@ Round up if number is as close to previous and next 0.5 steps.
 solution(4.75) == 5
 '''
 
+def solution(n):
+    part = n - int(n)
+    if 0.25 <= part < 0.75:
+        return int(n) + 0.5
+    elif part >= 0.75:
+        return int(n) + 1
+    else:
+        return int(n)
+		
+		
+def solution(n):
+    return round(2 * n) / 2
