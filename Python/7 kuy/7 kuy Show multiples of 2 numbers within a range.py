fdb8b80708e7dca272a97e57d89a8a6d03d41656
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-09-02 22:49:27 +0300

    2 sep 1

--- Python/7 kuy/7 kuy Show multiples of 2 numbers within a range.py
+++ Python/7 kuy/7 kuy Show multiples of 2 numbers within a range.py
@@ -9,6 +9,10 @@ Do NOT worry about checking zeros or negative values.
 To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
 '''
 
-
+def multiples(s1,s2,s3):
+    more_num = max(s1, s2)
+    less_num = min(s1, s2)
+    res = [num for num in range(more_num, s3, more_num) if num % less_num == 0]
+    return res
 			
 
