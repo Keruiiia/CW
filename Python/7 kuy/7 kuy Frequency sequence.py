aba729b80964cdd19d2ef6a8d9dbbd2bfe6f9015
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-09-03 21:29:10 +0300

    3 sep 2

--- Python/7 kuy/7 kuy Frequency sequence.py
+++ Python/7 kuy/7 kuy Frequency sequence.py
@@ -2,12 +2,17 @@
 Your task is to return an output string that translates an input string s by replacing each character
 in s with a number representing the number of times that character occurs
 in s and separating each number with the sep character(s).
+
+"hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+"19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+"^^^**$"     , "x" --> "3x3x3x2x2x1"
 '''
 
-def multiples(s1,s2,s3):
-    more_num = max(s1, s2)
-    less_num = min(s1, s2)
-    res = [num for num in range(more_num, s3, more_num) if num % less_num == 0]
-    return res
+def freq_seq(s, sep):
+    res, d = [], {}
+    for char in s:
+        d[char] = s.count(char)
+        res.append(str(d[char]))
+    return sep.join(res)
 			
 
