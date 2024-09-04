a79c2430ce9a2650181c02954e74f00259406747
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-09-04 23:01:44 +0300

    4 sep 2

--- Python/7 kuy/7 kuy Sum a list but ignore any duplicates.py
+++ Python/7 kuy/7 kuy Sum a list but ignore any duplicates.py
@@ -5,11 +5,13 @@ For instance, for the list [3, 4, 3, 6] the function should return 10,
 and for the list [1, 10, 3, 10, 10] the function should return 4
 '''
 
-def freq_seq(s, sep):
-    res, d = [], {}
-    for char in s:
-        d[char] = s.count(char)
-        res.append(str(d[char]))
-    return sep.join(res)
+from collections import Counter
+
+def sum_no_duplicates(l):
+    res = 0
+    for k, v in Counter(l).items():
+        if v == 1:
+            res += k
+    return res
 			
 
