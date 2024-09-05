4753edf056576d9e627d7cc19683d779c8fd6940
Keruiiia <48660199+Keruiiia@users.noreply.github.com>
2024-09-05 21:33:09 +0300

    5 sep 2

--- Python/7 kuy/7 kuy Odd Ones Out!.py
+++ Python/7 kuy/7 kuy Odd Ones Out!.py
@@ -3,14 +3,11 @@ Challenge: You are given a list of numbers. The numbers each repeat a certain nu
 of times. Remove all numbers that repeat an odd number of times while
 keeping everything else the same.
 '''
-
 from collections import Counter
 
-def sum_no_duplicates(l):
-    res = 0
-    for k, v in Counter(l).items():
-        if v == 1:
-            res += k
+def odd_ones_out(numbers):
+    d = Counter(numbers)
+    res = [num for num in numbers if d[num] % 2 == 0]
     return res
 			
 
